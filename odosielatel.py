41d41e528a062e4049d7fd72e77205fe8d65a7c7
Patrik Baran <patrikbaran02@gmail.com>
2023-11-16 08:59:39 +0100

    keepalive, oprava ack

--- odosielatel.py
+++ odosielatel.py
@@ -14,22 +14,21 @@ to_resend = []
 
 
 class Packet:
-    ACK = 1
-    NACK = 2
-    KEEP_ALIVE = 4
-    SWAP = 8
+    ACK = 1 << 0
+    NACK = 1 << 1
+    KEEP_ALIVE = 1 << 2
+    FINISH = 1 << 3
+    SWAP = 1 << 4
     WINDOW_SIZE = 8
 
-    RTT = 5  # todo menit?
+    RTT = 1  # todo menit?
 
-    def __init__(self, end_index, flags, number, message):
+    def __init__(self, flags, number, message):
         self.flags = flags
         self.number = number  # poradie % Packet.WINDOW_SIZE
         self.checksum: int = CRC(message)
         self.message = message
 
-        # znaci cast pokial obsahuje text
-        self.end_index = end_index
         self.ack = False
 
         self.timer = threading.Timer(Packet.RTT, self.resend)
@@ -69,13 +68,13 @@ class Packet:
 class Sender:
     def __init__(self, host, port, message, is_file, file_name=None):
         self.message = message.encode("utf-8")
-        print(self.message)
-        print(len(self.message))
+        self.last_sent_index = 0
+
         self.is_file = is_file
         self.file_name = file_name
 
         # pri inicializacii mame packet oznacujuci zaciatok spravy na 0. byte
-        self.datagrams = [Packet(0, 0, 0, b'')]
+        self.datagrams = []
 
         # todo upravit nazov
         #  tento socket odosiela a prijima, je v kontakte s prijimatelom
@@ -87,8 +86,10 @@ class Sender:
         self.selector.register(self.dest_socket, selectors.EVENT_WRITE | selectors.EVENT_READ)
         # self.selector.register(self.dest_socket, selectors.EVENT_READ)
 
-        self.is_alive = [True * 3]
+        self.is_alive = [True] * 3
         # todo start Keep Alive
+        self.alive_thread = threading.Thread(target=self.keep_alive)
+        self.alive_thread.start()
 
     def get_number_packet(self, number):
         i = 0
@@ -99,7 +100,7 @@ class Sender:
         return i
 
     def send(self):
-        packet_size = 500
+        packet_size = 1  # todo packet size
         i = 0
 
         # todo pridat odosielanie Keep Alive
@@ -107,25 +108,27 @@ class Sender:
         #  rtt viem zistit aj z ack/nack
 
         # todo zmenit while loop
-        while self.datagrams[-1].end_index < len(self.message):
+        # todo while loop neposle outstanding resend packets
+        while self.last_sent_index < len(self.message):
             events = self.selector.select()
             for key, mask in events:
                 if mask & selectors.EVENT_READ:
                     conn = key.fileobj
 
                     # dostaneme naspat hlavicku s nulovym checksum
-                    message = conn.recv(packet_size)
+                    message = conn.recv(4)
 
                     if message[0] & Packet.ACK:
                         # zistime ktory packet z okna sme prijali
                         k = self.get_number_packet(message[1])
                         packet = self.datagrams[k]
+                        packet.ack = True
 
                         packet.stop_timer()
 
                         # spocitame pocet packetov za sebou s ACK od prveho
                         j = 0
-                        while self.datagrams[j].ack and j < Packet.WINDOW_SIZE:
+                        while j < len(self.datagrams) and self.datagrams[j].ack:
                             j += 1
                         
                         # nasledne tieto packety odstranime z okna
@@ -152,24 +155,25 @@ class Sender:
                         # todo spytat sa presnejsie
                         pass
 
-                elif mask & selectors.EVENT_WRITE:
+                if mask & selectors.EVENT_WRITE:
                     conn = key.fileobj
                     if len(to_resend) != 0:
+                        print("resend")
                         # bud preposlem stary segment
                         with resend_lock:
                             item = to_resend.pop(0).out()
                         conn.send(item)
 
                     elif len(self.datagrams) < Packet.WINDOW_SIZE:
+                        print("write", [i.ack for i in self.datagrams])
                         # alebo poslem novy
-                        start = self.datagrams[-1].end_index
+                        start = self.last_sent_index
                         end = start + packet_size
                         # todo debug, na zaciatku su 2 divne byty
                         if i == 0:
-                            self.datagrams.pop(0)  # odstranime nulty segment
                             if self.is_file == 1:
                                 # pri subore vlozime nazov suboru
-                                print("FUCK")
+                                # todo odosielanie file_name po 1 byte
                                 end -= len(self.file_name) + 1
                                 message = Packet.file_header(self.file_name, self.message[:end])
                             else:
@@ -177,7 +181,8 @@ class Sender:
                         else:
                             message = self.message[start:end]
 
-                        self.datagrams.append(Packet(end, self.is_file << 7, i, message))
+                        self.datagrams.append(Packet(self.is_file << 7, i, message))
+                        self.last_sent_index += packet_size
 
                         conn.send(self.datagrams[-1].out())
 
@@ -194,29 +199,22 @@ class Sender:
 
         :return:
         """
-        sel = selectors.DefaultSelector()
-        sel.register(self.src_socket, selectors.EVENT_READ, data=None)
-
         interval = 5
         start_time = time.monotonic()
-        while True:
-            # start = time.monotonic()
+        while any(self.is_alive):
+            start = time.monotonic()
             self.dest_socket.send(bytes([4, 0, 0, 0]))
-            # detekuj odpoved
-            # popripade zapis neziskanu odpoved
-            # ak neziskas odpoved na 3, close connection
-
-            # pri select nastane cakanie do 1 s
-            # cakanie je blocking
-            # idk ako
-
-            # events = sel.select(timeout=1)
-            # for key, mask in events:
-            #     dest_socket.recv(508)
+            # print(self.is_alive, end="\t")
+            self.is_alive.pop(0)
+            self.is_alive.append(False)
+            # print(self.is_alive)
 
             time.sleep((interval - (time.monotonic() - start_time) % interval))
 
             # print(time.monotonic() - start)
+
+        # todo ako po ukonceni odosielania?
+        print("connection lost")
 
 
 def main():
