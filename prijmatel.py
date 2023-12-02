54e3126ffb6ff7a1c1de185bcda87fee9a869231
Patrik Baran <patrikbaran02@gmail.com>
2023-12-02 17:09:22 +0100

    priprava SWAP, lepsie okno

--- prijmatel.py
+++ prijmatel.py
@@ -2,9 +2,8 @@ import socket
 import time
 
 from CyclicRedundancyCheck import CRC
-from odosielatel import Sender
 from LDProtocol import LDProtocol
-
+import odosielatel
 
 class SelectiveRepeatARQ:
     def __init__(self):
@@ -12,16 +11,16 @@ class SelectiveRepeatARQ:
         self.is_file = False
         self.start = 0
         self.is_finalized = False
+        self.fragment_count = 0
 
-        # dict nie je sorted podla keys
-        # buffer na stare spravy
-        self.old_window = {i: None for i in range(LDProtocol.WINDOW_SIZE, LDProtocol.BUFFER_SIZE)}
+        # buffer na spravy
+        self.window = {i: None for i in range(LDProtocol.BUFFER_SIZE)}
 
-        # musime posunut az ked dostaneme nulty packet
-        self.current_window = {i: None for i in range(LDProtocol.WINDOW_SIZE)}
+        # oznacenie ktore spravy patria do aktualneho okna
+        self.current_window = [i for i in range(LDProtocol.WINDOW_SIZE)]
 
     def is_buffer_empty(self):
-        return all(i is None for i in self.current_window.values())
+        return all(self.window[i] is None for i in self.current_window)
 
     def check(self, frame: bytes):  # todo popracovat na nazvoch...
         header = frame[:4]
@@ -33,9 +32,15 @@ class SelectiveRepeatARQ:
         if (checksum ^ CRC(data)) != 0:
             return bytes([LDProtocol.NACK, seq, 0, 0])
 
-        if self.current_window.get(seq, False) is not False:
+        if seq in self.current_window:
+            if self.window[seq] is not None:
+                # todo co ak sme uz tento seq prijali?
+                # asi ack ak sa rovna, inak nack
+                pass
             # ak sa datagram nachadza v ocakavanom okne
 
+            # todo file flag pri zaciatku komunikácie
+            #  toto za bude menit pri zmene iniciacie komunikacue
             # pri prvok datagrame overime ci je sprava subor
             if self.message == b'' and not self.is_file:
                 self.start = time.monotonic()
@@ -44,28 +49,28 @@ class SelectiveRepeatARQ:
             self.current_window[seq] = data
             self.message += data
 
-            i = 0
-            first = next(iter(self.current_window))
-            last = next(reversed(self.current_window))
+            current_window_last = self.current_window[-1]
+            to_delete = 0
 
-            # todo treba lepsie?
-            keys = iter(self.current_window.values())
+            # najde dlzku retazca prijatych packetov od prveho v aktualnom okne
+            for i in self.current_window:
+                if self.window[i] is None:
+                    break
+                to_delete += 1
 
-            # odstrani od prveho retazove acky
-            # todo praca na vysvetleniach
-            while next(keys) is not None:
-                self.old_window.pop((first + i + LDProtocol.WINDOW_SIZE) % LDProtocol.BUFFER_SIZE)
-                self.old_window[(first + i) % LDProtocol.BUFFER_SIZE] = (
-                    self.current_window.pop((first + i) % LDProtocol.BUFFER_SIZE))
-                self.current_window[(last + i + 1) % LDProtocol.BUFFER_SIZE] = None
-                i += 1
+            # vsetky tieto packety ponechame v neaktivnej casti okna
+            # od najstarsieho packetu v starom okne
+            for i in range(to_delete):
+                self.current_window.pop(0)
+                new_current_window_end = (current_window_last + i) % LDProtocol.BUFFER_SIZE
+                self.window[new_current_window_end] = None
+                self.current_window.append(new_current_window_end)
 
-                keys = iter(self.current_window.values())
-
+            self.fragment_count += 1
             return bytes([LDProtocol.ACK, seq, 0, 0])
         else:
             # buffer je 2x okno, najdem packet s number, ak sa rovna, dostane ack, inak nack
-            old_data = self.old_window.get(seq)
+            old_data = self.window.get(seq)
 
             if data == old_data:
                 return bytes([LDProtocol.ACK, seq, 0, 0])
@@ -87,8 +92,11 @@ class SelectiveRepeatARQ:
 
 
 class Reciever:
-    def __init__(self, host, port):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def __init__(self, host=None, port=None, sock=None):
+        if sock is None:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            self.server = sock
         self.server.bind((host, port))
 
         self.debug = True  # "pokazi" prvy ukoncovaci packet
@@ -100,23 +108,34 @@ class Reciever:
             # todo spytat sa na velkost?
             #  508 je maximum co urcite nebude fragmentovane
             #    - toto zahrna nasu 4B hlavicku
+
+            # todo ako si ma "zmysliet" ze chce poslat swap?
             message, source = self.server.recvfrom(509)
+
             if message[0] & LDProtocol.KEEP_ALIVE:
+                # todo is_alive u prijemcu?
                 # posle spat Keep alive
                 self.server.sendto(bytes([LDProtocol.KEEP_ALIVE | LDProtocol.ACK, 0, 0, 0]), source)
                 print("recieved Keep Alive")
-            # elif message[0] & LDProtocol.SWAP:
-                # message = "ok".encode("utf-8")
+            elif message[0] & LDProtocol.SWAP:
+                self.server.sendto(bytes([LDProtocol.SWAP | LDProtocol.ACK, 0, 0, 0]), source)
 
-                # todo ako toto funguje?
-                #  ziskam ip od spravy ktoru ziskam?
-                #  od koho ma prist SWAP?
-                # Sender(host=self.host, port=self.port, message=message, sock=self.server.dup())
+                message = "swapped message"
+                # todo ak mame nieco na recieve, vsetko sa pokazi
+                #  2 moznosti
+                #   1. vyprazdnit bufffer
+                #   2. tu riesime swap od sendera. U sendera nastavime aby iba cital
+                #       ak nema co citat, akceptujeme swap a swapneme
+                #       -
+                #       swap od nas by znamenal fungovanie nadalej kym nedostaneme SWAP ACK
+                #       na strane odosielatela by sme iba spracovali vsetky odpovede a poslali SWAP ACP a swapli
+
+                # ip mam, je to source
+                novy = odosielatel.Sender(host=source[0], port=source[1], message=message, sock=self.server.dup())
+                novy.send()
             elif message[0] & LDProtocol.FIN:
-                # todo ak iba posledny packet zlyha
-                #  pri zlom crc nie je zaradeny do window
-                #  neviem ze som ho stratil
                 if self.debug:
+                    # ak je nastaveny debug, zmeni prvu spravu pre kontrolu CRC
                     message += b"PKS"
                     self.debug = False
 
@@ -132,12 +151,11 @@ class Reciever:
 
         # ukoncili sme spojenie, tak spracujeme spravu
         self.arq.output()
-        time.sleep(5)
 
 
 def main():
     HOST = '127.0.0.1'
-    # HOST = '147.175.160.168'
+    # HOST = ""  # na vsetkych adresach
     PORT = 9053  # int(input("Zadaj port komunikácie: "))
 
     recv = Reciever(HOST, PORT)
